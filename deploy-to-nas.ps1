d9258c7b1fb0af108a8f32490b99b4d9e5a783ba
mark.ku <mark.ku@ibuypower.com>
2024-10-19 14:30:19 +0800

    change for demo

--- deploy-to-nas.ps1
+++ deploy-to-nas.ps1
@@ -43,7 +43,5 @@ docker --tls -H="$containerUrl" load --input $imageFilePath
 # 建立及啟動容器應用
 docker --tls -H="$containerUrl" run -d --name $containerName --restart=always -p $port $imageName
 
-docker --tls -H="$containerUrl" run -d --name $containerName --restart=always -p $port $imageName
-
 pause 
 
