a1dd43b80d04afe1f5a3dc34788341e595e336c8
mark.ku <mark.ku@ibuypower.com>
2024-10-20 11:35:44 +0800

    remove no need code and change default lang

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,44 +6,18 @@ const nextIntlMiddleware = createMiddleware({
     locales: ['en', 'de'],
 
     // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-    defaultLocale: 'de',
+    defaultLocale: 'en',
 });
-
-export const config = {
-    // Skip all paths that should not be internationalized
-    matcher: ['/((?!api|_next|.*\\..*).*)'],
-};
 
 const REDIRECT_MAP: Record<string, { url: string; status: number }> = {
     // 308 permanent redirect
-    '/Login/Register': { url: '/register', status: 308 },
-    '/Login/Account': { url: '/account', status: 308 },
-    '/Login/Account/OrderHistory': { url: '/account/order-history', status: 308 },
-    '/Sale/PC-Konfigurator': { url: '/gaming-pcs', status: 308 },
-    '/support/wiederrufsbelehrung': { url: '/support/return-policy', status: 308 },
-    '/support/versandundzahlung': { url: '/support/payment-and-shipping', status: 308 },
-    '/support/reklamation': { url: '/support/warranty-policy', status: 308 },
-    '/Config/Ratenzahlung': { url: '/support/financing', status: 308 },
-    '/company': { url: '/about-us', status: 308 },
-    '/Company/Terms': { url: '/legal/terms', status: 308 },
-    '/Company/Privacy': { url: '/legal/privacy', status: 308 },
-    '/Company/Disclaimer': { url: '/legal/disclaimer', status: 308 },
-    '/Support/Sitemap': { url: '/sitemap', status: 308 },
-    '/support/widerrufsformular': { url: '/support/return-form', status: 308 },
-    '/carts/OrderSurvey': { url: '/carts/order-confirm', status: 308 },
-    '/gaming-pcs/ryzen-5000-series': { url: '/', status: 308 },
-    '/gaming-pcs/western-digital': { url: '/', status: 308 },
-    // 307 temporary redirect
-    '/gaming-pcs/13th-gen-core': { url: '/', status: 307 }, // TODO: change url after implement the page '/gaming-pcs/14th-gen-core'
-    '/Config/Search': { url: '/', status: 307 }, // TODO: change url after implement the page '/search',
+    // '/Config/Search': { url: '/', status: 307 }, // TODO: change url after implement the page '/search',
 };
 
 export default function middleware(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
 
     request.headers.set('x-url', request.nextUrl.href);
-
-    // request.headers.set('x-lang', request.url.includes('/en') ? 'en' : 'de');
 
     if (request.method === 'GET') {
         const redirectData = getRedirectData(pathname, REDIRECT_MAP);
